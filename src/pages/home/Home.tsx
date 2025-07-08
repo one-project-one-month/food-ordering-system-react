b68f1a665d17b1187bf4e80a99cd57b67da210b6
AUMGSOEMOE <soemoe2580aa@gmail.com>
2025-07-08 12:11:40 +0630

    shopSlide

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,11 @@
-const Home = () => {
-  return (
-    <div>
-      Home page
-    </div>
-  )
+import type { HomePageProps } from "../../types/homePage.types"
+import ShopList from "../../components/ShopList"
+
+
+const Home = ({homeType}:HomePageProps) => {
+  return <div>
+    <ShopList />
+  </div>
 }
 
 export default Home
