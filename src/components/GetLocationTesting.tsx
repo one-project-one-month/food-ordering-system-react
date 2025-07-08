2f5326cdbd3e39131de65cc7fa78ae143186c8da
Chan-Min-Thu <chanminthu961549167@gmail.com>
2025-07-09 00:37:10 +0700

    fixed extra content

--- src/components/GetLocationTesting.tsx
+++ src/components/GetLocationTesting.tsx
@@ -1,31 +1,11 @@
-import { Input } from './ui/input';
-import { Button } from './ui/button';
 import MapWithLeaf from './map/MapWithLeaf';
-import { getAddress } from '../services/apiGecoding';
 
 export default function GetLocationTesting() {
-  const getPosition = () => {
-    return new Promise<GeolocationPosition>((resolve, reject) => {
-      navigator.geolocation.getCurrentPosition(resolve, reject);
-    });
-  };
-  const handleGetLocation = async () => {
-    const position = await getPosition();
-    const { latitude, longitude } = position.coords;
-    const data = await getAddress({ latitude, longitude });
-    console.log('Address data:', data);
-    return data;
-  };
   return (
     <div className=" flex flex-col w-full h-auto gap-2">
-      <div>
-        <Input type="text" placeholder="Please fill your address." />
-        <Button type="button" onClick={handleGetLocation} variant="outline">
-          Subscribe
-        </Button>
-        <div className="w-full h-[500px]">
-          <MapWithLeaf />
-        </div>
+      <p>You can select custom address (OR) If you access GPS,you can easily click getLocation </p>
+      <div className="w-full h-[500px]">
+        <MapWithLeaf />
       </div>
     </div>
   );
