108ff4b4e0a185dbf7812471d172132c9f1153ee
Naing Lynn Aung <james342506@gmail.com>
2025-07-22 22:54:50 +0630

    integration Profile CRUD

--- src/types/ProfileType.ts
+++ src/types/ProfileType.ts
@@ -1,13 +1,10 @@
 export interface Profile {
     id?:number
-    userId: number;
     name:string;
     nrc?:string;
-    email:string;
     phone:string;
     dob?:string;
     gender?:string;
-    profilePic:string;
+    profilePic?:string;
     address:string;
-    count:number
 }
