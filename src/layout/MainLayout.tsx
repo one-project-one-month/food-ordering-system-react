8808845a010ea3953f25eb997186f9ce85d5ce38
Nu Nu Aung <nunuaung@Nu-Nu-Aungs-MacBook-Air.local>
2025-07-24 22:06:32 +0700

    restaurant crud and categories crud

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,16 +1,17 @@
 // import Header from "../Header"; // your header component
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
-
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const MainLayout = () => {
   return (
     <div className="w-full">
       <Header />
       <main className="mx-auto">
+        <ToastContainer position="top-right" className='top-[76px]' autoClose={2000} />
         <Outlet />
-      </main>
-       
+      </main>    
     </div>
   );
 };
--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,6 +1,29 @@
+import { useEffect } from "react"
 import DashboardCards from "../../components/DashboardCards"
+import { useDispatch } from "react-redux"
+import { type AppDispatch } from "../../store"
+import { getOwnerRestaurant } from "../../features/restaurant/restaurantSlice"
+import Cookies from "js-cookie"
 
 const Dashboard = () => {
+  const dispatch = useDispatch<AppDispatch>()
+  const getOwnerRestaurantData = async()=>{
+    try{
+      const result = await dispatch(getOwnerRestaurant())
+      if(result.payload.code===200){
+       if(result.payload.data.restaurants.length!==0){
+        Cookies.set('restaurantId',result.payload.data.restaurants[0].id as string)
+       }
+      }
+    }catch(e){
+      console.log("error ", e)
+    }
+  }
+
+  useEffect(()=>{
+    void getOwnerRestaurantData()
+  },[])
+
   return (
     <div>
         <div>
